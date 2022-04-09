8873a71a528348678132605a397c42b6e5a37a17
CMeyer19 <colemeyer95@outlook.com>
2022-04-09 20:50:35 +0200

    Next phase of work

--- controllers/deleteUser.ts
+++ controllers/deleteUser.ts
@@ -1,3 +1,21 @@
-export default function deleteUser() {
+import {deleteUser, getUser} from "../services/userService.ts";
 
-}
+export default async ({params, response}: { params: any, response: any }) => {
+    const userId = params.id;
+
+    if (!userId) {
+        response.status = 400;
+        response.body = {msg: "Invalid user ID"};
+        return;
+    }
+
+    const foundUser = await getUser(userId);
+    if (!foundUser) {
+        response.status = 404;
+        response.body = {msg: `User with ID ${userId} not found`};
+        return;
+    }
+
+    await deleteUser(userId);
+    response.body = {msg: "User deleted"};
+};
--- controllers/updateUser.ts
+++ controllers/updateUser.ts
@@ -1,3 +1,23 @@
-export default function updateUser() {
+import { updateUser } from "../services/userService.ts";
 
-}
+export default async ({ params, request, response }) => {
+    const userId = params.id;
+
+    if (!userId) {
+        response.status = 400;
+        response.body = { msg: "Invalid user ID" };
+        return;
+    }
+
+    if (!request.hasBody) {
+        response.status = 400;
+        response.body = { msg: "Invalid user data" };
+        return;
+    }
+
+    const { firstName, lastName } = await request.body().value;
+
+    await updateUser(userId, { firstName, lastName });
+
+    response.body = { msg: "User updated" };
+};
